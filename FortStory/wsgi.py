5e69d03fbebd679ff19e7f3005a2e96ccf71ec7a
101 <tmddnjs001133@dsm.hs.kr>
2024-01-29 15:35:04 +0900

    first commit

---
+++ FortStory/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for FortStory project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FortStory.settings')
+
+application = get_wsgi_application()
