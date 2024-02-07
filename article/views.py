f7bc2d056826c9afa08a0b7a69a0fbafe6d66b69
101 <tmddnjs001133@dsm.hs.kr>
2024-02-07 15:59:40 +0900

    s
    s

--- article/views.py
+++ article/views.py
@@ -1,21 +1,25 @@
 import json
 from django.shortcuts import render, redirect, get_object_or_404
 
-from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render
-
 from article.models import Article
 
 # Create your views here.
 from django.shortcuts import render, redirect
 
+
 def create_article(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
+
+        if title is None or content is None:
+            error_message = "제목과 내용을 모두 입력해주세요."
+            return render(request, 'error.html', {'error_message': error_message})
+
         article = Article(title=title, content=content)
         article.save()
         return redirect('view_article', articleId=article.id)
+
     return render(request, 'create_article.html')
 
 
@@ -24,6 +28,11 @@ def update_article(request, articleId):
     if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
+
+        if title is None or content is None:
+            error_message = "제목과 내용을 모두 입력해주세요."
+            return render(request, 'error.html', {'error_message': error_message})
+
         article.title = title
         article.content = content
         article.save()
