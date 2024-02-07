7c6a6a67663271cf34b67b1e13ceab7c64cd6608
101 <tmddnjs001133@dsm.hs.kr>
2024-02-07 15:09:05 +0900

    .

--- article/urls.py
+++ article/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from article import views
 
 urlpatterns = {
-    path('', views)
+    path('', views.create_article, name='create_article'),
+    path('', views.hello_world, name='hello_world'),
 }
